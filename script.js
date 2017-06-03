d263abf704e327ffab1ab83e923ea25e0856e249
dfenjves <fenjves.daniel@gmail.com>
2017-06-03 13:04:49 -0500

    removed solution

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
   // WRITE YOUR CODE IN HERE!
-  $('#one').click(function(){
-    $(this).css('background-color','blue');
-  })
 
-  $('#two').mouseenter(function(){
-    $(this).fadeOut();
-  })
 
 
 
