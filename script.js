74ed62a071ad1fd48943f5f6d9afa39c2a89c764
dfenjves <fenjves.daniel@gmail.com>
2017-06-03 13:04:20 -0500

    started solutions

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   // WRITE YOUR CODE IN HERE!
   $('#one').click(function(){
-    $(this).css('background-color','blue');
+    $('#one').css('background-color','blue');
   })
 
   $('#two').mouseenter(function(){
-    $(this).fadeOut();
+    $('#two').fadeOut();
   })
+
+  $('button')
 
 
 
