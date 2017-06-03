ace60b1eb95be990a1101abca87d9004d6ae1f64
dfenjves <fenjves.daniel@gmail.com>
2017-06-02 20:45:47 -0500

    working on solutions

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // WRITE YOUR CODE IN HERE!
+  $('#one').click(function(){
+    $(this).css('background-color','blue');
+  })
+
+  $('#two').mouseenter(function(){
+    $(this).fadeOut();
+  })
+
+
+
+})
